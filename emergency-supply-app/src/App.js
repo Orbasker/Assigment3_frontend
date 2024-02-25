0f8da0c20b6d4348a6630da8fba9dbb74d874ee5
Or Basker <or.basker@ridewithvia.com>
2024-02-25 22:33:57 +0200

    updates to package.json and deployed to render

--- emergency-supply-app/src/App.js
+++ emergency-supply-app/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./App.css";
 import ListInventory from "./components/ListInventory";
+// import MyComponent from "./components/myComponent";
+import MyComponent from "./template/MyComponent";
 
 const App = () => {
   return (
@@ -8,7 +10,7 @@ const App = () => {
       <h1 class="text-4xl font-bold text-center text-blue-600 my-4">
         Emergency Supply App
       </h1>
-
+      {/* <MyComponent /> */}
       <ListInventory />
     </div>
   );
